b0526a3d3a08d5364db1c67485972ee10f06fb84
Nick Bogle <nick.bogle@ziply.com>
2023-06-10 16:37:18 -0700

    UI Fixes

--- netbox_floorplan/urls.py
+++ netbox_floorplan/urls.py
@@ -3,7 +3,7 @@ from . import models, views
 from netbox.views.generic import ObjectChangeLogView
 
 urlpatterns = (
-    # path('test/', views.TestView.as_view(), name='svg'),
+    path('floorplans/test/', views.TestView.as_view(), name='svg'),
     path('floorplans/', views.FloorplanListView.as_view(), name='floorplan_list'),
     path('floorplans/add/', views.FloorplanEditView.as_view(), name='floorplan_add'),
     path('floorplans/<int:pk>/', views.FloorplanView.as_view(), name='floorplan'),
