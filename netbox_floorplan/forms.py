a175216bd007023b90bbd1b39425223eb3fe0c54
tbotnz <tonynealon1989@gmail.com>
2023-06-12 10:22:37 +1200

    Revert "commit"

--- netbox_floorplan/forms.py
+++ netbox_floorplan/forms.py
@@ -1,14 +1,7 @@
 from netbox.forms import NetBoxModelForm
 from .models import Floorplan, FloorplanObject
-from dcim.models import Rack
 
 class FloorplanForm(NetBoxModelForm):
         class Meta:
             model = Floorplan
-            fields = ['site', 'location', 'background_image', 'scale']
-
-class FloorplanRackFilterForm(NetBoxModelForm):
-      class Meta:
-            model = Rack
             fields = ['site', 'location', 'background_image', 'scale', 'measurement_unit']
-
--- netbox_floorplan/views.py
+++ netbox_floorplan/views.py
@@ -50,4 +50,3 @@ class FloorplanMapEditView(LoginRequiredMixin, View):
         #     existingracks.append(object.id)
         form = forms.FloorplanRackFilterForm
         return render(request, "netbox_floorplan/floorplan_edit.html", {"form": form, "site": site, "racklist": racklist})
-
