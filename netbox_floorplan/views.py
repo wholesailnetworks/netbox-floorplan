20d6d5dd6809e29cb216084f7e57fcfd351b9472
tbotnz <tonynealon1989@gmail.com>
2023-06-12 10:10:50 +1200

    Update views.py

--- netbox_floorplan/views.py
+++ netbox_floorplan/views.py
@@ -1,13 +1,16 @@
 from django.db.models import Count
 from netbox.views import generic
 from . import filtersets, forms, models, tables
+from dcim.models import Site, Rack
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, HttpResponseNotFound
 from django.views import View
 from django.shortcuts import render
+
+import json
+
 from utilities.views import ViewTab, register_model_view
 from dcim.models import Site
-
 
 class TestView(LoginRequiredMixin, View):
 
@@ -34,3 +37,16 @@ class FloorplanEditView(generic.ObjectEditView):
 
 class FloorplanDeleteView(generic.ObjectDeleteView):
     queryset = models.Floorplan.objects.all()
+
+
+class FloorplanMapEditView(LoginRequiredMixin, View):
+    def get(self, request, pk):
+        site = Site.objects.get(id=pk)
+        racklist = Rack.objects.filter(site=site)
+        objectlist = models.Floorplan.objects.get(site=site.id)
+        # existingracks = []
+        # canvas = json.loads(objectlist.canvas)
+        # for object in canvas:
+        #     existingracks.append(object.id)
+        form = forms.FloorplanRackFilterForm
+        return render(request, "netbox_floorplan/floorplan_edit.html", {"form": form, "site": site, "racklist": racklist})
